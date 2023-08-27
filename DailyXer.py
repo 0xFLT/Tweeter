b9b7605433fdcbda926954761f27c32ba3941726
FreeLunchTrading <64173645+0xFLT@users.noreply.github.com>
2023-08-27 14:30:14 -0500

    Update DailyXer.py

--- DailyXer.py
+++ DailyXer.py
@@ -3,32 +3,36 @@ import openai
 import requests
 from bs4 import BeautifulSoup
 
-#Twitter API keys
-consumer_key = 'CONSUMER_KEY'
-consumer_secret = 'CONSUMER_SECRET'
-access_token = 'ACCESS_TOKEN'
-access_secret = 'ACCESS_SECRET'
+# Set up Twitter API keys
+consumer_key = 'YOUR_CONSUMER_KEY'
+consumer_secret = 'YOUR_CONSUMER_SECRET'
+access_token = 'YOUR_ACCESS_TOKEN'
+access_secret = 'YOUR_ACCESS_SECRET'
 
-#OpenAI API key
+# Set up OpenAI API key
 openai.api_key = 'YOUR_OPENAI_API_KEY'
 
-#Authenticate Twitter API
+# Authenticate with Twitter API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
-#Prompt for URL link
+# Read tweet dataset from a text file named 'AllTimeHits.txt'
+with open('AllTimeHits.txt', 'r') as file:
+    tweets_dataset = file.read().splitlines()
+
+# Prompt for a URL link
 article_url = input("Enter the URL of the article: ")
 
-#Fetch article content
+# Fetch article content from the URL
 response = requests.get(article_url)
 soup = BeautifulSoup(response.content, 'html.parser')
 article_content = soup.get_text()
 
-#Define prompt
+# Define the prompt
 prompt = "Write the post to inspire and educate developers and crypto/blockchain enthusiasts in a technical but relatable way:\n\n"
 
-#Train language model using the prompt, tweets dataset, and article content
+# Train the language model using the prompt, tweets dataset, and article content
 training_data = prompt + "\n".join(tweets_dataset) + "\n" + article_content
 response = openai.Completion.create(
     engine="text-davinci-003",
@@ -37,9 +41,9 @@ response = openai.Completion.create(
     temperature=0.7
 )
 
-#Generate tweet
+# Generate a new tweet using the trained model
 new_tweet = response.choices[0].text.strip()
 
-#Post generated tweet
+# Post the generated tweet on Twitter
 api.update_status(new_tweet)
 print("Tweet posted:", new_tweet)
